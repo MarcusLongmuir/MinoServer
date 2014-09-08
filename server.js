cef338f4dbb0c7e10904c96ac9000d1ba8855726
Marcus Longmuir <marcuslongmuir@me.com>
2014-09-08 21:03:31 +0100

    Initial code commit

---
+++ server.js
@@ -0,0 +1,51 @@
+var logger = require('tracer').console();
+var express = require('express');
+var connect = require('connect');
+var http = require('http');
+var path = require('path');
+
+var MinoDB = require('minodb');
+var mino = new MinoDB({
+    api: true,
+    ui: true,
+    db_address: 'mongodb://127.0.0.1:27017/minodb'
+})
+
+mino.add_field_type({
+	name: "custom_field",
+	display_name: "Custom Field",
+	class: require('./custom_fields/MyCustomRuleField')
+})
+
+var server = express();
+server.set('port', process.env.PORT || 5002);
+server.set('views', path.join(__dirname, 'views'));
+server.set('view engine', 'jade')
+server.use(express.static(path.join(__dirname, '../public')));;
+server.use(express.errorHandler());
+
+server.use('/api/', mino.api_server())
+server.use('/ui/', mino.ui_server())
+
+// mino.api.connect(function(){
+// 	mino.api.call({username:"TestUser"},{
+// 		"function": "get",
+// 		parameters:{
+// 			addresses:[
+// 				"/TestUser/People/Marcus Longmuir",
+// 				"person"
+// 			]
+// 		}
+// 	},function(err,res){
+// 		logger.log(err);
+// 		logger.log(JSON.stringify(res,null,4));
+// 	})
+// })
+
+server.get('/*', function(req, res) {
+    res.render('index');
+})
+
+http.createServer(server).listen(server.get('port'), function() {
+    console.log('Server started on port ' + server.get('port'));
+});
